4146c2a753b25fa2daf43698017bc0d56b9b5b7e
Saneksah <74658991+Saneksah@users.noreply.github.com>
2020-11-18 15:18:53 +0300

    1234
    1234

---
+++ zadacha2.js
@@ -0,0 +1,12 @@
+if (process.argv[2]>0)
+{
+	console.log(process.argv[3]);
+} 
+else if (process.argv[2]<0)
+{
+	console.log(process.argv[3].split('').reverse().join(''));
+}
+else 
+{
+	console.log("undefined behavior");
+}
